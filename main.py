f6b2c060dd5d9f2d94b87103664c53f53fd1fd80
Hugo Aguiar <hugoaguiarpassos@gmail.com>
2025-10-14 23:35:07 -0300

    Inicializando o projeto do primeiro jogo

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+window = pygame.display.set_mode((800, 600))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
